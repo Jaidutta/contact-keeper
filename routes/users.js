bbec2729d196192a1d9c9baea4f92e031e051a06
Joy Dutta <dutta_joy@hotmail.com>
2020-11-05 09:08:02 +0000

    adds User Model, validation, hash password

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,80 @@
 const express = require('express');
-const router = express.Router()
+const router = express.Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const { check, validationResult } = require('express-validator/check');
 
-//  route   POST  api/users
-//  @desc   Register a user
-//  Public
-router.post('/', (req, res) => {
-  res.send('Registers a user')
-})
+const User = require('../models/User');
 
-module.exports = router
+// @route    POST api/users
+// @desc     Register user
+// @access   Public
+router.post(
+	'/',
+	[
+		check('name', 'Name is required')
+			.not()
+			.isEmpty(),
+		check('email', 'Please include a valid email').isEmail(),
+		check(
+			'password',
+			'Please enter a password with 6 or more characters'
+		).isLength({ min: 6 })
+	],
+	async (req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
+
+		const { name, email, password } = req.body;
+
+		try {
+			let user = await User.findOne({ email });
+
+			if (user) {
+        return res.status(400).json({ msg: 'User already exists' });
+      // 400 bad request: user did not fulfil the requirement for the request
+
+			}
+
+			user = new User({
+				name,
+				email,
+				password
+			});
+
+			const salt = await bcrypt.genSalt(10);
+
+			user.password = await bcrypt.hash(password, salt);
+
+			await user.save();
+
+			const payload = {
+				user: {
+					id: user.id
+				}
+			};
+
+			jwt.sign(
+				payload,
+				config.get('jwtSecret'),
+				{ expiresIn: 360000 },
+				(err, token) => {
+					if (err) throw err;
+					res.json({ token });
+				}
+			);
+		} catch (err) {
+			console.error(err.message);
+      res.status(500).send('Server error');
+      // 500 is a server error
+		}
+	}
+);
+
+module.exports = router;
+
+
+
